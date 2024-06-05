1e882f545d70aa18fc6dedb493a657ad817344b7
Bilgehan Meryem Özmen <bilgehan.ozmen@boun.edu.tr>
2024-06-05 09:12:54 +0300

    LatesttVersionn

---
+++ Controllers/Dashboard.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoable.Controllers
+{
+    public class Dashboard : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -45,7 +45,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
 app.MapRazorPages();
 
